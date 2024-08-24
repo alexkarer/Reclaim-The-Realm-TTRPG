5dcb2775f5d9a949e88e3a6e731ba05da90ac45d
Alexander Karer <alex.karer@gmail.com>
2024-08-24 23:02:07 +0200

    add basic framework for spells and all descriptions and rules explanations

--- src/app/rules-reference/spells/spells.component.ts
+++ src/app/rules-reference/spells/spells.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { DynamicContentComponent } from "../../shared/dynamic-component-rendering/dynamic-content.component";
+import { KeywordProcessorPipe } from "../../shared/text-transformer/text-transformer";
+import { NgbScrollSpyFragment } from '@ng-bootstrap/ng-bootstrap';
+
+import spellsJson from '../../../../ttrpg_resources/spells/spells.json';
 
 @Component({
   selector: 'app-spells',
   standalone: true,
-  imports: [],
+  imports: [DynamicContentComponent, KeywordProcessorPipe, NgbScrollSpyFragment],
   templateUrl: './spells.component.html',
   styleUrl: './spells.component.scss'
 })
 export class SpellsComponent {
-
+  public readonly spellsJson = spellsJson;
 }
