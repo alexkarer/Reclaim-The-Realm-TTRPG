2d4934ad87342b44109badddec1c447014b36804
Alexander Karer <alex.karer@gmail.com>
2024-09-07 02:02:58 +0200

    finish spells, start with hybrid abilities

--- src/app/rules-reference/hybrid-abilities/hybrid-abilities.component.ts
+++ src/app/rules-reference/hybrid-abilities/hybrid-abilities.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { KeywordProcessorPipe } from "../../shared/text-transformer/text-transformer";
+import { DynamicContentComponent } from "../../shared/dynamic-component-rendering/dynamic-content.component";
+
+import hybridAbilitiesJson from '../../../../ttrpg_resources/hybrid_abilities/hybrid_abilities.json';
 
 @Component({
   selector: 'app-hybrid-abilities',
   standalone: true,
-  imports: [],
+  imports: [DynamicContentComponent, KeywordProcessorPipe],
   templateUrl: './hybrid-abilities.component.html',
   styleUrl: './hybrid-abilities.component.scss'
 })
 export class HybridAbilitiesComponent {
-
+  public hybridAbilityDescription = hybridAbilitiesJson.hybridAbilitiesDescription;
 }
